aaf3a662936ebf859bc3dbc9764e0f38c6caa2c8
cat2151 <5794738+cat2151@users.noreply.github.com>
2025-11-19 22:46:27 +0900

    毎回の開発用installを楽にするためのrust-scriptを用意した

---
+++ install-ym2151-tools.rs
@@ -0,0 +1,9 @@
+#!/usr/bin/env rust-script
+// cargo-deps: duct
+use duct::cmd;
+fn main() {
+    cmd!("cat-play-mml", "--shutdown").run().ok();
+    for tool in ["ym2151-log-play-server", "cat-play-mml", "cat-edit-mml", "ym2151-tone-editor"] {
+        cmd!("cargo", "install", "--git", format!("https://github.com/cat2151/{}", tool)).run().unwrap();
+    }
+}
