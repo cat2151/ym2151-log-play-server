dac0301dd2aa32f0efb0ea02ff1e51d9b5973042
cat2151 <5794738+cat2151@users.noreply.github.com>
2025-11-22 00:05:04 +0900

    今なにをinstall中かわかりやすくした

--- install-ym2151-tools.rs
+++ install-ym2151-tools.rs
@@ -4,6 +4,7 @@ use duct::cmd;
 fn main() {
     cmd!("cat-play-mml", "--shutdown").run().ok();
     for tool in ["ym2151-log-play-server", "cat-play-mml", "cat-edit-mml", "ym2151-tone-editor"] {
+        println!("Installing {}...", tool);
         cmd!("cargo", "install", "--git", format!("https://github.com/cat2151/{}", tool)).run().unwrap();
     }
 }
